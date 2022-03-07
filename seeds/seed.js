8aeeb1e915f8a97fb5e884d2aff6657d5301300f
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-06 21:05:06 -0500

    setup

---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const { User, Posts } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force:true});
+    
+    const users = await User.bulkCreate(userData,{
+        individualHooks:true,
+        returning:true,
+    });
+
+    
+}
