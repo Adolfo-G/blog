0590ffcacc19a2cd825189e983d36c0d7b091b9b
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-28 03:15:35 -0400

    css and display changes

--- models/comment.js
+++ models/comment.js
@@ -1,41 +1,41 @@
-const {Model, DataTypes}=require('sequelize');
+const { Model, DataTypes, Sequelize } = require('sequelize');
 const sequelize = require('../config/connection')
 
-class Comment extends Model{}
+class Comment extends Model { }
 
 Comment.init({
-    id:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey:true,
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
     },
-    content:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    user_id:{
-        type:DataTypes.INTEGER,
-        references:{
-            model:'user',
-            key:'id'
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
         }
     },
-    post_id:{
-        type:DataTypes.INTEGER,
-        references:{
-            model:'posts',
-            key:'id'
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'posts',
+            key: 'id'
         }
-    }
+    },
 },
-{
-    sequelize,
-    timestamps:false,
-    freezeTableName:true,
-    underscored:true,
-    modelName:'comment'
-}
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
 )
 
-module.exports=Comment;
+module.exports = Comment;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,21 +1,43 @@
 const commentBtn = document.querySelector('.comment')
+const deleteCommentBtns = document.querySelectorAll('.delete')
 
 const newComment = async () => {
-    const title = document.querySelector('#header').getAttribute('title');
-    const postUserId = document.querySelector('#header').getAttribute('user');
-    const comment = document.querySelector('#post-body').value
-    
-    const response = await fetch('/api/comments', {
-        method: 'POST',
-        body: JSON.stringify({ title, postUserId, comment }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
-  };
+  const title = document.querySelector('#header').getAttribute('title');
+  const postUserId = document.querySelector('#header').getAttribute('user');
+  const comment = document.querySelector('#post-body').value
 
-  commentBtn.addEventListener('click', newComment)
+  const response = await fetch('/api/comments', {
+    method: 'POST',
+    body: JSON.stringify({ title, postUserId, comment }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+};
+
+commentBtn.addEventListener('click', newComment)
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/comments/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      document.location.reload()
+      //alert('Failed to delete comment');
+    }
+  }
+};
+
+for (let i = 0; i < deleteCommentBtns.length; i++) {
+  deleteCommentBtns[i].addEventListener('click', delButtonHandler);
+}
